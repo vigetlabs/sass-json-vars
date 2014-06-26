0c4021670704865cbeff07bc921029284a83df64
Nate Hunzaker <nate.hunzaker@gmail.com>
2014-06-26 08:34:03 -0400

    Address issues with namespacing. Bump to 0.3.1

--- lib/sass-json-vars/importer.rb
+++ lib/sass-json-vars/importer.rb
@@ -1,52 +1,55 @@
 require 'sass'
 require 'json'
 
-class SassJSONVars::Importer < Sass::Importers::Filesystem
+module SassJSONVars
+  class Importer < Sass::Importers::Filesystem
 
-  def watched_file?(uri)
-    uri =~ /\.json$/ &&
-      uri.start_with?(root + File::SEPARATOR)
-  end
+    def watched_file?(uri)
+      uri =~ /\.json$/ &&
+        uri.start_with?(root + File::SEPARATOR)
+    end
 
-  protected
+    protected
 
-  def extensions
-    {'json' => :scss}
-  end
+    def extensions
+      {'json' => :scss}
+    end
 
-  def json?(name)
-    File.extname(name) == '.json'
-  end
+    def json?(name)
+      File.extname(name) == '.json'
+    end
 
-  private
+    private
 
-  def _find(dir, name, options)
-    return unless json? name
+    def _find(dir, name, options)
+      return unless json? name
 
-    full_filename, syntax = Sass::Util.destructure(find_real_file(dir, name, options))
-    return unless full_filename && File.readable?(full_filename)
+      full_filename, syntax = Sass::Util.destructure(find_real_file(dir, name, options))
+      return unless full_filename && File.readable?(full_filename)
 
-    json      = JSON.parse(IO.read(full_filename))
-    variables = json.map { |key, value| "$#{key}: #{_convert_to_sass(value)}" }.join("\n")
+      json      = JSON.parse(IO.read(full_filename))
+      variables = json.map { |key, value| "$#{key}: #{_convert_to_sass(value)}" }.join("\n")
 
-    Sass::Engine.new(variables)
-  end
+      Sass::Engine.new(variables)
+    end
 
-  def _convert_to_sass(item)
-    if item.is_a? Array
-      _make_list(item)
-    elsif item.is_a? Hash
-      _make_map(item)
-    else
-      item
+    def _convert_to_sass(item)
+      if item.is_a? Array
+        _make_list(item)
+      elsif item.is_a? Hash
+        _make_map(item)
+      else
+        item
+      end
+    end
+
+    def _make_list(item)
+      '(' + item.map { |i| _convert_to_sass(i) }.join(',') + ')'
+    end
+
+    def _make_map(item)
+      '(' + item.map {|key, value| key.to_s + ':' + _convert_to_sass(value) }.join(',') + ')'
     end
   end
 
-  def _make_list(item)
-    '(' + item.map { |i| _convert_to_sass(i) }.join(',') + ')'
-  end
-
-  def _make_map(item)
-    '(' + item.map {|key, value| key.to_s + ':' + _convert_to_sass(value) }.join(',') + ')'
-  end
 end
--- lib/sass-json-vars/version.rb
+++ lib/sass-json-vars/version.rb
@@ -1,3 +1,3 @@
 module SassJSONVars
-  VERSION = "0.2.1"
+  VERSION = "0.3.1"
 end
